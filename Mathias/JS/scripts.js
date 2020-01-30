98a9f388e82a48927afb2c1d29b55cbe3ea57749
Mathias Schoettle <39705097+MathiasSchoettle@users.noreply.github.com>
2020-01-30 23:26:26 +0100

    started work on a user review section

--- Mathias/JS/scripts.js
+++ Mathias/JS/scripts.js
@@ -1,11 +1,10 @@
 function openOrCloseMenu() {
   var dropdownMenu = document.getElementById('mainMenuDropDownItems');
 
-  if(dropdownMenu.style.visibility == 'hidden' || dropdownMenu.style.visibility == 0){
+  if (dropdownMenu.style.visibility == 'hidden' || dropdownMenu.style.visibility == 0) {
     dropdownMenu.style.visibility = 'visible'
     dropdownMenu.style.height = "200px";
-  }
-  else {
+  } else {
     dropdownMenu.style.visibility = 'hidden';
     dropdownMenu.style.height = "0px";
   }
