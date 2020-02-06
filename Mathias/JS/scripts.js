b86e2161f1a4a160af264a9306bc57cd5d054fb8
Mathias Schoettle <39705097+MathiasSchoettle@users.noreply.github.com>
2020-02-06 21:14:31 +0100

    reimagined user-review section and added majas critics section

--- Mathias/JS/scripts.js
+++ Mathias/JS/scripts.js
@@ -1,44 +1,3 @@
- document.addEventListener("DOMContentLoaded", function() {
-
-   var children = document.getElementById('user-review-star-container').children
-   var backup = [];
-
-   for(let i = 0; i < children.length; i++){
-     backup.push(children[i].innerText);
-   }
-
-   for (let i = 0; i < children.length; i++) {
-     children[i].addEventListener('mouseover', function() {
-       var check = false;
-       for (let i = 0; i < children.length; i++) {
-         if (!check) {
-           if (children[i] != this) {
-             children[i].innerText = 'star';
-           } else {
-             check = true;
-             this.innerText = 'star';
-           }
-         } else {
-           children[i].innerText = 'star_border';
-         }
-       }
-     });
-
-     children[i].addEventListener('mouseleave', function() {
-       for(let i = 0; i < children.length; i++){
-         children[i].innerText = backup[i];
-       }
-     });
-
-     children[i].addEventListener('click', function() {
-      for(let i = 0; i < children.length; i++){
-        backup[i] = children[i].innerText;
-      }
-     });
-   }
-
- });
-
  function openOrCloseMenu() {
    var dropdownMenu = document.getElementById('mainMenuDropDownItems');
    var icon = document.getElementById('menubtnico');
@@ -65,44 +24,92 @@
    document.getElementById('trailer-video').currentTime = 0;
  }
 
- function galleryScrollRight() {
-
-   document.getElementById('scrollRight').style = "pointer-events: none";
-
-   let gallery = document.getElementById('gallery').querySelectorAll('.gallery-container');
-   let classes = [];
-
-   for(let i = 0; i < gallery.length; i++){
-     classes.push(gallery[i].classList[1]);
-   }
-
-   classes.unshift(classes.pop());
-
-   for(let i = 0; i < gallery.length; i++){
-     gallery[i].classList.remove(gallery[i].classList[1]);
-     gallery[i].classList.add(classes[i]);
-   }
-
-   setTimeout(() => {document.getElementById('scrollRight').style = "pointer-events: auto";}, 1000);
- }
-
- function galleryScrollLeft() {
-
-   document.getElementById('scrollLeft').style = "pointer-events: none";
+function galleryScroll(isRight, id) {
+   document.getElementById(id).style = "pointer-events: none"; // disable button
 
    let gallery = document.getElementById('gallery').querySelectorAll('.gallery-container');
    let classes = [];
 
    for(let i = 0; i < gallery.length; i++){
-     classes.push(gallery[i].classList[1]);
+     classes.push(gallery[i].classList[1]); //store classes of images in order
    }
 
-   classes.push(classes.shift());
+   isRight ? classes.unshift(classes.pop()) : classes.push(classes.shift()); // left or right shift classes
 
    for(let i = 0; i < gallery.length; i++){
-     gallery[i].classList.remove(gallery[i].classList[1]);
+     gallery[i].classList.remove(gallery[i].classList[1]);// replace classses with shifted ones
      gallery[i].classList.add(classes[i]);
    }
 
-   setTimeout(() => {document.getElementById('scrollLeft').style = "pointer-events: auto";}, 1000);
- }
+   setTimeout(() => {document.getElementById(id).style = "pointer-events: auto";}, 1000); // enable button after the transition has ended
+}
+
+function submitForm() {
+  if(checkReviewText() & checkReviewName() & checkReviewScore()){
+    document.getElementById('user-review-container').style.maxHeight = '0';
+    document.getElementById('user-review-container').style.padding = '0';
+  }
+}
+
+function checkReviewScore(){
+  let score = document.getElementById('full-stars').offsetWidth;
+  let star = document.getElementById('review-hover-container');
+
+  if(score <= 0){
+    star.style.color = '#ff4242';
+    return false;
+  } else {
+    star.style.color = '#ededed';
+    return true;
+  }
+}
+
+function checkReviewName() {
+  let name = document.getElementById('review-name');
+
+  if(name.value.trim() == ''){
+    name.style.borderColor = '#ff4242';
+    return false;
+  } else {
+    name.style.borderColor = '#ededed';
+    return true;
+  }
+}
+
+function checkReviewText() {
+  let text = document.getElementById('review-text');
+
+  if(text.value.trim() == ''){
+    text.style.borderColor = '#ff4242';
+    return false;
+  }
+  else {
+    text.style.borderColor = '#ededed';
+    return true;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var score = 0;
+
+  var stars = document.getElementById('border-stars');
+  stars.onmousemove = function(e) {
+      var rect = document.getElementById('border-stars').getBoundingClientRect();
+      var x = e.pageX - parseInt(rect.left);
+      document.getElementById('full-stars').style.width = x + 'px';
+  }
+
+  stars.addEventListener('mouseleave', function() {
+    document.getElementById('full-stars').style.transition = '0.6s';
+    document.getElementById('full-stars').style.width = score + 'px';
+  });
+
+  stars.addEventListener('mouseenter', function() {
+    document.getElementById('full-stars').style.transition = '0.1s';
+  });
+
+  stars.addEventListener('click', function() {
+    score = document.getElementById('full-stars').offsetWidth;
+  })
+});
